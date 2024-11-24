1a5b196ab432a6222a4b1e681f773e4cee4e79a9
Sebastiano Del Gobbo <sebdg@tiscali.it>
2024-11-24 18:19:35 +0100

    ADD: Polyglot.Desktop: Utilizzo di un ListBox per la visualizzazione delle voci del menù principale

--- Polyglot.Desktop/UI/MainWindow/MainWindow.xaml.cs
+++ Polyglot.Desktop/UI/MainWindow/MainWindow.xaml.cs
@@ -4,8 +4,13 @@ namespace Polyglot.Desktop.UI.MainWindow;
 
 public partial class MainWindow : Window
 {
+	private MainWindowVM _vm;
+
 	public MainWindow()
 	{
 		this.InitializeComponent();
+
+		this._vm = new MainWindowVM();
+		this.DataContext = this._vm;
 	}
 }
---
+++ Polyglot.Desktop/UI/MainWindow/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Polyglot.Desktop.UI.MainMenu;
+using System.Collections.ObjectModel;
+
+namespace Polyglot.Desktop.UI.MainWindow;
+
+public partial class MainWindowVM : ObservableObject
+{
+	/// <summary>
+	/// Restituisce o imposta l'elenco delle voci che costituiscono il menù principale
+	/// </summary>
+	public ObservableCollection<PgtMainMenuItem> MainMenuItems { get; set; }
+
+	/// <summary>
+	/// Costruttore
+	/// </summary>
+	public MainWindowVM()
+	{
+		// Creazione dell'elenco delle voci che costituiscono il menù principale
+		this.MainMenuItems = new ObservableCollection<PgtMainMenuItem>()
+		{
+			new PgtMainMenuItem("\uE80F", "Home"),
+			new PgtMainMenuItem("\uF2B7", "Languages"),
+			new PgtMainMenuItem("\uE8F2", "Translations"),
+		};
+	}
+}
